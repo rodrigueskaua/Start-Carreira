83091412f33717141c23a86adb12b0f1df6dbeb1
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-04-26 16:45:41 -0300

    fix(sidebar.js): Correção do fechamento automático e o efeito blur
    Corrige um problema no arquivo sidebar.js, onde o menu estava sendo fechado automaticamente de forma incorreta. Além disso, foi corrigido o comportamento do efeito de blur aplicado conteúdo fora do sidebar quando a largura da tela era menor que 768 pixels. Agora, o menu funciona corretamente e o efeito de blur é aplicado conforme esperado.

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,22 +1,39 @@
-const showMenu = (headerToggle, navbarId) => {
+const showMenu = (headerToggle, navbarId, contentId) => {
   const toggleBtn = document.getElementById(headerToggle);
   const nav = document.getElementById(navbarId);
+  const content = document.querySelector('.main-page');
 
   if (toggleBtn && nav) {
-    toggleBtn.addEventListener('click', () => {
-      nav.classList.toggle('show-menu');
-      toggleBtn.classList.toggle('bx-x');
-    });
+      toggleBtn.addEventListener('click', () => {
+          nav.classList.toggle('show-menu');
+          toggleBtn.classList.toggle('bx-x');
+          content.classList.toggle('blur');
+      });
 
-    document.addEventListener('click', (event) => {
-      const target = event.target;
-      const isSideBarOpen = nav.classList.contains('show-menu');
+      document.addEventListener('click', (event) => {
+          const target = event.target;
+          const isSideBarOpen = nav.classList.contains('show-menu');
 
-      if (isSideBarOpen && !target.closest('#' + navbarId) && !target.closest('#' + headerToggle)) {
-        nav.classList.remove('show-menu');
-        toggleBtn.classList.remove('bx-x');
-      }
-    });
+          if (isSideBarOpen && !target.closest('#' + navbarId) && !target.closest('#' + headerToggle)) {
+              nav.classList.remove('show-menu');
+              toggleBtn.classList.remove('bx-x');
+              content.classList.remove('blur');
+          }
+      });
+
+      nav.addEventListener('mouseover', () => {
+          if (window.innerWidth > 768) {
+              content.classList.add('blur');
+          }
+      });
+
+      nav.addEventListener('mouseout', () => {
+          if (window.innerWidth > 768) {
+              nav.classList.remove('show-menu');
+              toggleBtn.classList.remove('bx-x');
+              content.classList.remove('blur');
+          }
+      });
   }
 };
 
